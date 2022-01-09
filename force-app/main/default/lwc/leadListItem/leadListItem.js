cd5dad30db9f72edf18dcba48b1bed3c6a6c653c
Harish Sappa <harishvenkatsai@gmail.com>
2022-01-09 17:22:24 +0400

    LWC Learning

---
+++ force-app/main/default/lwc/leadListItem/leadListItem.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class LeadListItem extends LightningElement {
+    @api lead;
+
+}
+
---
+++ force-app/main/default/lwc/myFirstLWC/myFirstLWC.js
@@ -0,0 +1,10 @@
+import { LightningElement, track } from 'lwc';
+
+export default class MyFirstLWC extends LightningElement {
+    @track searchTerm;
+
+    handleNewSearch(event) {
+        this.searchTerm = event.target.value; 
+    }
+
+}
